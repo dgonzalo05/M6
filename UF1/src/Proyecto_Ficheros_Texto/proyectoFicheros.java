68a2e0791ccead5142dfb9028b19c5485862fe7b
dgonzalo05 <dgonzalo@iesjoandaustria.org>
2018-12-11 19:41:38 +0100

    proyecto ficheros

--- UF1/src/Proyecto_Ficheros_Texto/proyectoFicheros.java
+++ UF1/src/Proyecto_Ficheros_Texto/proyectoFicheros.java
@@ -3,57 +3,51 @@ package Proyecto_Ficheros_Texto;
 import java.io.*;
 import java.util.*;
 public class proyectoFicheros {
-
-    public static String treure_coma(String campo){
-        String no_coma = "";
-        for (int lletra = 0; lletra < campo.length(); lletra++){
-            if (campo.charAt(lletra) != ','){
-                no_coma += campo.charAt(lletra);
-            }
-        }
-        return no_coma;
-    }
     
 	public static void main(String[] args) throws IOException {
+		// Definimos todas las variables
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         BufferedReader etiquetes = null;
         BufferedReader dades = null;
         BufferedWriter xml = null;
-        BufferedWriter log = null;
         int con = 1;
-        int valor;
         String linea, etiqueta;
+        ArrayList<String> etiquetas = new ArrayList<String>();
         String salt = "\n";
-        String sep = "\t";
+        String tab = "\t";
+        String sep = ",";
         String[] datos;
         try{
-            etiquetes = OpenFile.r("file1.txt");
-            dades = OpenFile.r("file2.txt");
-            xml = OpenFile.w("file3.txt");
-            log = OpenFile.a("fitxer_log.log");
-
+        	// Abrimos ficheros
+            etiquetes = OpenFile.r("file1.csv");
+            dades = OpenFile.r("file2.csv");
+            xml = OpenFile.w("file3.xml");
+            
+            // Las etiquetas hola y adios (0) y (1)
+            etiquetas.add(etiquetes.readLine());
+            etiquetas.add(etiquetes.readLine());
+            
             xml.write("<doc>"+salt);
             
             while((linea=dades.readLine())!=null){
                 datos = linea.split(sep);
+                xml.write(tab+"<elem nr=\""+con+"\">"+salt);
                 for (int i = 0; i < datos.length; i++){
-                	etiqueta = etiquetes.readLine();
-                    xml.write(sep+"<elem nr=\""+con+"\">"+salt);
-                    while((linea=dades.readLine())!=null){
-                    	xml.write(sep+sep+"<"+etiqueta+">"+linea.charAt(i)+"</"+etiqueta+">");
-                    }
-
-                    xml.write(salt+sep+"</elem>"+salt);
-                    con++;
+                	if (etiquetas.size() > i) {
+                		etiqueta = etiquetas.get(i);
+                	} else {etiqueta = "altre";}
+                    xml.write(tab+tab+"<"+etiqueta+">"+datos[i]+"</"+etiqueta+">"+salt);
                 }
+                xml.write(tab+"</elem>"+salt);
+                con++;
             }
             xml.write("</doc>");
         }catch (IOException e){
             System.out.println(e.getMessage());
         } finally {
         	etiquetes.close();
+        	dades.close();
         	xml.close();
-        	log.close();
         }
     }
 }
