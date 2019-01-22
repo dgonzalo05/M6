3bb23dc018fb60f6edfb1ac89c0b6cd45c6c0895
dgonzalo05 <dgonzalo@iesjoandaustria.org>
2019-01-22 19:13:38 +0100

    Hibernate II

---
+++ UF1/src/Main.java
@@ -0,0 +1,33 @@
+import primero.*;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import java.util.Date;
+
+public class Main {
+	public static void main(String[] args) {
+		//En primer lugar se obtiene la sesión creada por el Singleton.
+		SessionFactory sesion = HibernateUtil.getSessionFactory();
+		//Abrimos sesión e iniciamos una transacción
+		Session session = sesion.openSession();
+		Transaction tx = session.beginTransaction();
+		System.out.println("Inserto una fila en emple");
+		//Creamos un nuevo objeto Depart y damos valor a sus atributos
+		// Depart dep = new Depart();
+		Date fecha = new Date();;
+		Emple emp = new Emple();
+		emp.setEmpNo((byte) 7727);
+		emp.setDepart(new Depart(10));
+		emp.setApellido("GONZALO");
+		emp.setOficio("PROGRAMADOR");
+		emp.setDir(7698);
+		emp.setFechaAlt(fecha);
+		emp.setDir(7698);
+		emp.setSalario(1364);
+		//Guardamos en la base de datos y comprometemos la información
+		session.save(emp);
+		tx.commit();
+		session.close();
+		System.exit(0);
+	}
+}
